e28a6c62d0c4c212e017be56fe86c3156e68fb77
coderboi88 <thecoderboy88@gmail.com>
2022-03-29 11:25:58 +0530

    "First Commit"

---
+++ src/main/java/com/aditya/springsecurityclientside/SpringSecurityClientSideApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.springsecurityclientside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/springsecurityclientside/SpringSecurityClientSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.springsecurityclientside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClientSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
